0439f47ff184733f96fab53bb66aef0da8a332a6
Fred Ma <fredma1234@gmail.com>
2022-12-31 16:59:01 -0500

    Added mod6-ex-mybank

---
+++ mod6-ex-mybank/src/main/scala/com/fma/spring/BankApplication6.scala
@@ -0,0 +1,13 @@
+package com.fma.spring
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+object BankApplication6 {
+  def main(args: Array[String]): Unit = {
+    SpringApplication.run(classOf[BankApplication6])
+  }
+}
+
+@SpringBootApplication
+class BankApplication6
